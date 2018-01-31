60d9833349529de0ac99a1b4fd401877d22eb0b9
TonyYuta <tonykagaya@gmail.com>
2018-01-30 19:58:05 -0800

    ws

--- src/main/java/com/toolsqa/restful/apirestassured/GetWeather.java
+++ src/main/java/com/toolsqa/restful/apirestassured/GetWeather.java
@@ -4,9 +4,9 @@ package com.toolsqa.restful.apirestassured;
  * HWeather. Get methods
  *
  */
-public class GetWeather{
+public class GetWeather {
 	
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("GetWeather class");
     }
     
