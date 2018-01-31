721472a78f26cb3ec4044fa352d883872387b872
TonyYuta <tonykagaya@gmail.com>
2018-01-30 20:48:04 -0800

    tab removed & Enter put

--- src/main/java/com/toolsqa/restful/apirestassured/GetWeather.java
+++ src/main/java/com/toolsqa/restful/apirestassured/GetWeather.java
@@ -7,7 +7,7 @@ package com.toolsqa.restful.apirestassured;
 public class GetWeather {
 
     public static void main(String[] args) {
-    	   final String arg = args[0];
-    	      System.out.println("GetWeather class. Arg: " + arg);
+       final String arg = args[0];
+          System.out.println("GetWeather class. Arg: " + arg);
     	      }
     }
