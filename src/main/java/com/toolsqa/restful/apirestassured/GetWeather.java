5d07d29486f9b30d82a31ef34ffaf40d8547eb34
TonyYuta <tonykagaya@gmail.com>
2018-01-30 20:52:37 -0800

    all tabs were delete

--- src/main/java/com/toolsqa/restful/apirestassured/GetWeather.java
+++ src/main/java/com/toolsqa/restful/apirestassured/GetWeather.java
@@ -9,5 +9,5 @@ public class GetWeather {
     public static void main(String[] args) {
        final String arg = args[0];
           System.out.println("GetWeather class. Arg: " + arg);
-    	      }
+          }
     }
